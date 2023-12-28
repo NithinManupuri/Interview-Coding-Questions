2d42f3dc40f5a3f159f3771f4a0497e0f786bccb
NithinManupuri <manupurinithin@gmail.com>
2023-12-28 14:21:50 +0530

    hackpass modified

--- HackPass.java
+++ HackPass.java
@@ -2,16 +2,28 @@ package Ibm;
 
 public class HackPass {
     public static void main(String[] args){
-
+        allCombinatons("","1234");
     }
-    public static void allCombinatons(String num){
-
-           char ch=(char)(num.charAt(0)+96);
-        for(int i=0;i<num.length();i++){
-
+    public static void allCombinatons(String pass, String s) {
+        if (s.isEmpty()) {
+            System.out.print(pass);
+            return;
         }
 
 
-       // allCombinatons(int num);
+        for (int i = 0; i < s.length(); i++) {
+            if (i == 0) {
+                char ch = (char) (s.charAt(0) + 96);
+            } else {
+                char ch = (char) (s.charAt(i) + s.charAt(i - 1));
+                if (ch > 122) {
+                    allCombinatons(pass + s.charAt(i), s.substring(2));
+                    i++;
+
+                }
+                allCombinatons(pass + ch, s.substring(1));
+
+            }
+        }
     }
 }
