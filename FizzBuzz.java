e93a178a99cef690830c0609a97e9e6ff2d0ef9f
ArunKumaryerragopula <143619540+ArunKumaryerragopula@users.noreply.github.com>
2024-03-01 10:21:34 +0530

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -18,7 +18,7 @@ public class FizzBuzz {
                 System.out.println("Fizz");
             }
             if(i%3!=0 && i%5==0){
-                System.out.println("Fizz");
+                System.out.println("Buzz");
             }
             if(i%3!=0 && i%5!=0){
                 System.out.println(i);
